dc2d4a1d1f8a692487781bd1edda376014199d8c
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-12-13 07:15:26 +0530

    first push

--- popup.php
+++ popup.php
@@ -8,13 +8,26 @@
 		$headers="From: ".$email;
 
 		if(mail($to, $subject, $message, $headers)){
-			echo "<h1 style=\"text-align:center\">Sent Successfully! Thank you"." ".$email.", We will contact you shortly!</h1>";
-			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1bRynYudHLMevU-HG8PmdhqrGWHglwfnh">Click here to download Brochure</a><h3>';
+		$apiKey = urlencode("PC56D9fMjaQ-3gw9h0qV4d5KkwYZMpo5xr1McpRBYv");
+		// Message details
+		$numbers = array(918050933931);
+		$sender = urlencode("TXTLCL");
+		$message = rawurlencode("$message");
+
+		$numbers = implode(",", $numbers);
+
+		// Prepare data for POST request
+		$data = array("apikey" => $apiKey, "numbers" => $numbers, "sender" => $sender, "message" => $message);
+		// Send the POST request with cURL
+		$ch = curl_init("https://api.textlocal.in/send/");
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		echo "<h1 style=\"text-align:center\">Sent Successfully! Thank you"." ".$email.", We will contact you shortly!</h1>";
+		echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1bRynYudHLMevU-HG8PmdhqrGWHglwfnh">Click here to download Brochure</a><h3>';
 		}
-		else{
-			echo "Something went wrong!";
-		}
-	
 	}
 ?>
 
